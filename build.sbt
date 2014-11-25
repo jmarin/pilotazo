9726747dec0bdf2a6d1990d815f069c4097013b7
Juan Marin Otero <juan.marin.otero@gmail.com>
2014-11-25 15:49:06 -0500

    Simple Persistence with View

--- build.sbt
+++ build.sbt
@@ -4,14 +4,20 @@ version := "0.0.1"
 
 scalaVersion := "2.11.4"
 
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+
 libraryDependencies ++= {
   val parserCombinators = "1.0.2"
   val spray = "1.3.1"
+  val akka = "2.3.7"
   val specs2 = "2.4.2"
   val scalacheck = "1.11.6"
   Seq(
     "org.scala-lang.modules" %% "scala-parser-combinators" % parserCombinators,
     "io.spray" %%  "spray-json" % spray,
+    "com.typesafe.akka" %% "akka-persistence-experimental" % akka,
+    "com.typesafe.akka" %% "akka-cluster" % akka,
+    "com.github.ironfish" %% "akka-persistence-mongo-casbah" % "0.7.5-SNAPSHOT",
     "org.specs2" %% "specs2" % specs2 % "test",
     "org.scalacheck" %% "scalacheck" % scalacheck % "test"
   )
--- src/main/scala/Pilotazo.scala
+++ src/main/scala/Pilotazo.scala
@@ -10,30 +10,27 @@ import persistence.Cmd
 object Pilotazo extends LARSplitter {
 
   def main(args: Array[String]) {
-    //val lines = Source.fromFile("src/main/resources/lar.dat").getLines().toList
+    val lines = Source.fromFile("src/main/resources/lar.dat").getLines().toList
     //val lines = Source.fromFile("/Users/marinj/Downloads/hmda_exports/bankofthewest.dat").getLines.toList
-    val lines = Source.fromFile("/Users/marinj/Downloads/hmda_exports/arvestmortgagecompany.dat").getLines.toList
+    //val lines = Source.fromFile("/Users/marinj/Downloads/hmda_exports/arvestmortgagecompany.dat").getLines.toList
+    //val lines = Source.fromFile("/Users/marinj/Downloads/hmda-complete.dat").getLines.toList
     val tsLine = lines.head
     val ts = TransmittalSheetReader(tsLine)
     val larLines = lines.tail
     var lars = larLines.map { line =>
       println(line)
-      println(split(line))
       LARReader(line)
     }
     val filespec = FileSpec(ts, lars)
-    //println(filespec)
+    println(filespec)
 
-    //val system = ActorSystem("hmda")
-    //val persistentActor = system.actorOf(Props(classOf[ExamplePersistenceActor]), "persistentActor-1")
-    //val viewActor = system.actorOf(Props(classOf[MyView]), "view-1")
+    val system = ActorSystem("hmda")
+    val persistentActor = system.actorOf(Props(classOf[ExamplePersistenceActor]), "persistentActor-1")
+    val viewActor = system.actorOf(Props(classOf[MyView]), "view-1")
 
-    //lars.foreach { lar =>
-    //  lar match {
-    //    case Some(l) => persistentActor ! Cmd(l.toString)
-    //    case None =>
-    //  }
-    //}
+    lars.foreach { lar =>
+      persistentActor ! Cmd(lar.toString)
+    }
 
     //Thread.sleep(1000)
     //system.shutdown()
